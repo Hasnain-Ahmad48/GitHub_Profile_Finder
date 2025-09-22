74be882b8bf631cb297c797f6a575570b33e0bf3
Hasnain-Ahmad48 <hasnaink1480@gmail.com>
2025-09-22 15:28:22 +0500

    hanain

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import GithubProfileFinder from './components'
+import {useState} from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import GithubProfileFinder from "./components";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <GithubProfileFinder/>
+      <GithubProfileFinder />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
